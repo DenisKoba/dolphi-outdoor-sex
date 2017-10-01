8c03aa3c64de3c3370ba41b7804151e1d526f9de
griffin <griffin@zeoalliance.com>
2017-10-01 22:19:03 +0300

    01.10/4

--- scripts.js
+++ scripts.js
@@ -97,3 +97,8 @@ $(document).ready(function () {
 
 })
 
+$(document).ready(function() {
+    $("body").children().each(function() {
+        $(this).html($(this).html().replace(/&#8232;/g," "));
+    });
+});
